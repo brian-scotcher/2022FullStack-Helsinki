119a699205fd39337d74e03c9812b760febffa2c
Brian <brian.scotcher@gmail.com>
2022-05-06 22:10:56 +0300

    1.10 UniCafe Submission
    Each Statistic given its own component as StatisticLine and Button Handler refactored. Unnecessary code removed.

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,93 +1,72 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Title = ({titles}) => {
-  return<h2>{titles}</h2>  
-}
+const Title = ({ titles }) => {
+  return <h2>{titles}</h2>;
+};
 
-const Statistics = ({ good, neutral, bad,}) => {
-  
-  const noVotes= "No feedback given"
-
-  //constants for stats
-  const totalVotes= good+neutral+bad //toal number of votes
-  const averageVotes = (good-bad)/totalVotes //average number of votes between +1 and -1
-  const positivePercentage = good/totalVotes*100 //Percentage of positive votes
-
-  if(totalVotes===0){
-    return(
-    <div>
-      {noVotes}
-    </div>
-    )
-  }else{
+//Component for each individual statistic
+const StatisticLine = ({ text, value }) => {
   return (
     <div>
-    Good: {good} <br/>
-    Neutral: {neutral} <br/>
-    Bad: {bad}<br/>
-    All: {totalVotes}<br/>
-    Average: {averageVotes}<br/>
-    Possitive: {positivePercentage}%
+      {text} {value}
     </div>
-  )
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const noVotes = "No feedback given";
+
+  //constants for stats
+  const totalVotes = good + neutral + bad; //toal number of votes
+  const averageVotes = (good - bad) / totalVotes; //average number of votes between +1 and -1
+  const positivePercentage = (good / totalVotes) * 100 + "%"; //Percentage of positive votes
+
+  if (totalVotes === 0) {
+    return <div>{noVotes}</div>;
+  } else {
+    return (
+      <div>
+        <StatisticLine text="Good: " value={good} />
+        <StatisticLine text="Neutral: " value={neutral} />
+        <StatisticLine text="Bad: " value={bad} />
+        <StatisticLine text="Total: " value={totalVotes} />
+        <StatisticLine text="Average: " value={averageVotes} />
+        <StatisticLine text="Positive: " value={positivePercentage} />
+      </div>
+    );
   }
-}
+};
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-
-
+//Button Component
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
 const App = () => {
-  
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  //Click Handlers
+  const goodClick = () => setGood(good + 1);
+  const neutralClick = () => setNeutral(neutral + 1);
+  const badClick = () => setBad(bad + 1);
 
   const titles = {
-    feedback: 'Give Feedback',
-    stats: 'Statistics'
-  }
-
-  const setToGood = newValue => {
-    console.log('Good Responses', newValue)
-    setGood(newValue)
-  }
-
-  const setToNeutral = newValue => {
-    console.log('Neutral Responses', newValue)
-    setNeutral(newValue)
-  }
-
-  const setToBad = newValue => {
-    console.log('Bad Responses', newValue)
-    setBad(newValue)
-  }
-
-  
-
+    feedback: "Give Feedback",
+    stats: "Statistics",
+  };
 
   return (
     <div>
       <Title titles={titles.feedback} />
-      <Button handleClick={() => setToGood(good +1)} text="Good" />
-      <Button handleClick={() => setToNeutral(neutral+1) } text="Neutral" />
-      <Button handleClick={() => setToBad(bad +1)} text="Bad" />
+      <Button onClick={goodClick} text="Good" />
+      <Button onClick={neutralClick} text="Neutral" />
+      <Button onClick={badClick} text="Bad" />
 
       <Title titles={titles.stats} />
-      <Statistics
-      good={good}
-      neutral={neutral}
-      bad={bad}
-      />
-
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
