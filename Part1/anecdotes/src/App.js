eca9430a6e175842e912e62be724dbf144d8782d
Brian <brian.scotcher@gmail.com>
2022-05-11 17:28:08 +0300

    1.13 Anecdotes submission
    1.13 Anecdotes Step 2 submission. Vote button added. Also displays number of votes for each anecdote.

--- Part1/anecdotes/src/App.js
+++ Part1/anecdotes/src/App.js
@@ -3,7 +3,18 @@ import { useState } from "react";
 //Button Component
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
+const AnecdoteDisplayed = ({ text, votesDisplayed }) => {
+  return (
+    <div>
+      {text}
+      <br />
+      has {votesDisplayed} votes.
+    </div>
+  );
+};
+
 const App = () => {
+  //Array of Anecdotes
   const anecdotes = [
     "If it hurts, do it more often",
     "Adding manpower to a late software project makes it later!",
@@ -14,18 +25,32 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
   ];
 
+  //Hooks
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0)); //ensures the array will always be able to handle any number of anecdotes
 
+  //Random Number generated from length of anecdotes array
   const randomNumber = Math.floor(Math.random() * anecdotes.length);
 
   //Click Handlers
   const anecdoteClick = () => setSelected([randomNumber]);
+  const voteClick = () => {
+    //create copy of votes array to add a new vote
+    const newVote = [...votes];
+    //Adds the new vote to the selected anecdote
+    newVote[selected] += 1;
+    //Uses the newVote array with the newly added vote and replaces the votes so newVotes is now votes
+    setVotes(newVote);
+  };
 
   return (
     <div>
-      {anecdotes[selected]}
-      <br />
+      <AnecdoteDisplayed
+        text={anecdotes[selected]}
+        votesDisplayed={votes[selected]}
+      />
 
+      <Button onClick={voteClick} text="Vote" />
       <Button onClick={anecdoteClick} text="Next Anecdote" />
     </div>
   );
